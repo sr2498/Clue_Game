60cb82935a72ace22da01685f601bcf90276187c
Sandhya <134464080+sr2498@users.noreply.github.com>
2026-06-30 01:20:33 -0400

    Refactor comment separator in knowledge_base.py

--- knowledge_base.py
+++ knowledge_base.py
@@ -19,7 +19,7 @@ class KnowledgeBase:
         # Cards players might have (inferred from refutations)
         self.maybe_by_player = {p: set() for p in self.players}
 
-    # -----------------------------------------------------------------
+    # ----------------------------------------------------------------------
     def mark_seen(self, player, card):
         for category in self.possible:
             self.possible[category].discard(card)
