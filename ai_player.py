7bb47e2f49bf2ac1f9e55d640c36940c9ab1da11
Sandhya <134464080+sr2498@users.noreply.github.com>
2026-07-13 12:56:37 -0400

    Refactor comment formatting in ai_player.py
    Standardized comment section formatting in ai_player.py.

--- ai_player.py
+++ ai_player.py
@@ -14,9 +14,9 @@ class AIPlayer:
     def init_kb(self, all_characters, all_weapons, all_rooms, my_cards):
         self.kb = KnowledgeBase(all_characters, all_weapons, all_rooms, my_cards)
 
-    # -----------------------
+    # ---------------------
     # MOVEMENT DECISION
-    # -----------------------
+    # ---------------------
     def pick_move(self):
         pos = characters[self.name]["position"]
         moves = valid_moves(pos)
@@ -35,9 +35,9 @@ class AIPlayer:
         # Otherwise, move randomly among valid paths
         return random.choice(moves)
 
-    # -------------------------
+    # -----------------------
     # MAKING A SUGGESTION
-    # -------------------------
+    # -----------------------
     def make_suggestion(self):
         pos = characters[self.name]["position"]
 
@@ -76,8 +76,8 @@ class AIPlayer:
     def process_no_refute(self, suggester, players_checked, suggestion_triplet):
         self.kb.note_no_refutation(suggester, players_checked, suggestion_triplet)
 
-    # --------------------
+    # ------------------
     # ACCUSATION LOGIC
-    # --------------------
+    # ------------------
     def consider_accusation(self):
         return self.kb.single_solution_candidate()
